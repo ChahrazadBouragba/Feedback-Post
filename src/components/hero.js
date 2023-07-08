08625e2d040b96514ed6374aa11c9a2999e4c8e7
ChahrazadBouragba <chbouragba25@gmail.com>
2023-07-08 20:21:37 +0100

    updating hero section

---
+++ src/components/hero.js
@@ -0,0 +1,9 @@
+import img from '../img/hero/plate-transformed.png'
+
+export default function Hero() {
+    return (
+        <div className="hero-div">
+            <img className="hero-img" src={img}></img>
+        </div>
+    )
+}
